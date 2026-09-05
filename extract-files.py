fec98405174b3a207786d8b3e73be83e59832b7c
CuriousNom <aryanbiswas7585@gmail.com>
2026-09-04 21:08:54 -0700

    temp
    Change-Id: I012307e1ff56a665ee31e5adfbf837aaa2bec052

--- extract-files.py
+++ extract-files.py
@@ -64,9 +64,6 @@ blob_fixups: blob_fixups_user_type = {
 }  # fmt: skip
 
 namespace_imports = [
-    'hardware/qcom-caf/common/libqti-perfd-client',
-    'hardware/qcom-caf/sm8250',
-    'vendor/qcom/opensource/display',
     'vendor/xiaomi/sm8250-common',
 ]
 
